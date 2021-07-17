39c8297fb90369203a91d66729488b50fa27d60a
shaik_peeramohidin <87254914+sk-pm@users.noreply.github.com>
2021-07-17 19:17:08 +0530

    Update calbmi.py
    Third commit

--- calbmi.py
+++ calbmi.py
@@ -1,47 +1,60 @@
-#import streamlit
+'''
+Program to calculate BMI
+BMI=Weight in KG/ square of height in meters.
+1Pound(Lb)=0.453592 Kilograms(Kgs)
+Author:Shaik Peeramohidin
+GitHub:https://github.com/sk-pm
+'''
+#importing  streamlit
 import streamlit as st
 #title to app
-st.title("Welcome : BMI Calculator")
-#take input
-weight = st.number_input("Please Enter your weight (in KGs)")
-#take height
-status = st.radio('Please select your height format: ',('cms', 'meters', 'feet'))
-#compare status value
+st.title("Welcome - BMI Calculator")
+#take weight units
+weight_units=st.radio('Please Select Weight In Units:',('Kilograms(Kgs)','Pounds(lbs)'))
+#weight
+try:
+    weight = st.number_input("Please Enter Your Weight :")
+#convert weight pounds to Kgs
+    if weight_units == 'Pounds(lbs)' :
+        weight = weight*0.453592
+    else :
+        weight=weight * 1
+except:
+    st.text("Above field can not be blank")
+#take height units
+height_units = st.radio('Please Select Your Height Format: ',('Centimeters', 'Meters', 'Feets', 'Inchs'))
+height = st.number_input("Please Enter Your Height :")
+#convert to meters
 #bmi fomula weight/(hight)^2
-if(status == 'cms'):
-    height = st.number_input('Centermeters')
-    try:
-        bmi=weight/((height/100)**2)
-    except:
-        st.text("Enter some value of height")
-elif(status =='meters'):
-    height = st.number_input('Meters')
-    try:
-        bmi=weight/(height**2)
-    except:
-        st.text("Enter some value of height")
-else:
-    height = st.number_input('Feet')
-    try:
-        #1mt=3.28
-        bmi=weight/(((height/3.28))**2)
-    except:
-        st.text("Enter some value of height")
+try:
+    if(height_units == 'Centimeters'):
+        height = height/100 #centimeters to meters
+    elif(height_units =='Feets'):
+        height = height/3.28 #feets to meters
+    elif(height_units =='Inchs'):
+        height = height * 0.0254
+    else:
+        height = height * 1
+except:
+    st.text("Above field can not be blank")
 
-if(st.button('Calculate BMI')):
- try:
-    #print bmi index
-    st.text("Your BMI Index is {}".format(bmi))
-    if(bmi < 16):
-        st.error("You are Extremely Underweight")
-    elif(bmi >=16 and bmi <18.5):
-        st.warning("Ypu are Under weight")
-    elif(bmi >=18.5 and bmi < 25):
-        st.success("Healthy")
-    elif(bmi > 25 and bmi < 30):
-        st.warning("Overweight")
-    elif(bmi>=30):
-        st.error("Extremly Overweight")
- except:
-    st.text("Please enter the values.")
-st.text("----------By Peeramohidin")
+
+def calculate_bmi(h, w):
+    return w / h * h
+try:
+    if(st.button('Calculate BMI')):
+
+        bmi = calculate_bmi(height,weight)
+        st.text(("You BMI is {}".format(bmi)))
+        if(bmi < 16):
+            st.error("You are Extremely Underweight")
+        elif(bmi >= 16 and bmi <18.5):
+            st.warning("You are Underweight")
+        elif(bmi >= 18.5 and bmi < 25):
+            st.success("You are Heatlhy(Normal)")
+        elif(bmi >=25 and bmi < 30):
+            st.success("You are Overweight")
+        elif(bmi >= 30):
+            st.error("You are Extremly Overweight")
+except:
+    st.text("Please make sure all fields are filled")
